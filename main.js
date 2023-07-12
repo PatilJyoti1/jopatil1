c6c0d74794f09837793c59f4049d79ba0eabbafb
PatilJyoti1 <jyotipatil616861@gmail.com>
2023-07-12 19:10:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+function reverseName(name){
+    return name.split('').reverse().join('');
+
+}
+
+
+var name = "jyoti patil";
+var reversedName = reverseName(name);
+console.log(reversedName);
+
+
+
+
+
+
